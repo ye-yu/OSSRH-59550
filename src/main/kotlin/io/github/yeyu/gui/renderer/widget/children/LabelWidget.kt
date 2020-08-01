ee80d39dc306e1f1fe10f3a27ca270a6bf84a733
ye-yu <rafolwen98@gmail.com>
2020-08-01 21:23:51 +0800

    Use Text on label

--- src/main/kotlin/io/github/yeyu/gui/renderer/widget/children/LabelWidget.kt
+++ src/main/kotlin/io/github/yeyu/gui/renderer/widget/children/LabelWidget.kt
@@ -3,9 +3,11 @@ package io.github.yeyu.gui.renderer.widget.children
 import io.github.yeyu.gui.renderer.ScreenRenderer
 import io.github.yeyu.gui.renderer.widget.ChildWidget
 import io.github.yeyu.gui.renderer.widget.ParentWidget
+import io.github.yeyu.gui.renderer.widget.ParentWidget.AnchorType
 import io.github.yeyu.util.DrawerUtil
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.util.math.MatrixStack
+import net.minecraft.text.Text
 
 /**
  * A static label widget
@@ -14,8 +16,11 @@ class LabelWidget(
     override val relativeX: Int = 0,
     override val relativeY: Int = 0,
     private val color: Int = DrawerUtil.constructColor(0xFF, 0xFF, 0xFF, 0xFF),
-    private val label: String,
-    override val name: String
+    private val label: Text,
+    override val name: String,
+    horizontalAnchor: AnchorType = AnchorType.START,
+    verticalAnchor: AnchorType = AnchorType.START,
+    private val shadow: Boolean
 ) : ChildWidget {
 
     private var parent: ParentWidget? = null
@@ -32,6 +37,16 @@ class LabelWidget(
     override val width: Int
         get() = renderer.getWidth(label)
     override val height: Int = renderer.fontHeight
+    private val offsetX = when(horizontalAnchor) {
+        AnchorType.START -> 0
+        AnchorType.MIDDLE -> -(width / 2.0).toInt()
+        AnchorType.END -> -width
+    }
+    private val offsetY = when(verticalAnchor) {
+        AnchorType.START -> 0
+        AnchorType.MIDDLE -> -(height / 2.0).toInt()
+        AnchorType.END -> -height
+    }
 
     override fun render(
         matrices: MatrixStack,
@@ -41,13 +56,23 @@ class LabelWidget(
     ) {
         val drawX = getDrawX()
         val drawY = getDrawY()
-        renderer.draw(
-            matrices,
-            label,
-            drawX.toFloat(),
-            drawY.toFloat(),
-            color
-        )
+        if (shadow) {
+            renderer.drawWithShadow(
+                matrices,
+                label,
+                drawX.toFloat() + offsetX,
+                drawY.toFloat() + offsetY,
+                color
+            )
+        } else {
+            renderer.draw(
+                matrices,
+                label,
+                drawX.toFloat() + offsetX,
+                drawY.toFloat() + offsetY,
+                color
+            )
+        }
     }
 
     override fun setFocused(focused: Boolean) {
